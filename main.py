c7d5e89bb12a55004d76e8310844747a78108c69
legiordano <lucasgiordano2219@gmail.com>
2023-07-04 23:42:50 -0300

    Fix connection

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import openai
 
-openai.api_key = 'tu_clave_de_api'
+openai.api_key = 'api_key'
 
-def obtener_respuesta(consulta):
+def get_response(query):
     response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=consulta,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
+    engine='text-davinci-003',
+    prompt=query,
+    max_tokens=100,
+    n=1,
+    stop=None,
+    temperature=0.7
     )
-    
-    respuesta = response.choices[0].text.strip()
-    
-    return respuesta
 
-consulta_usuario = input("Escribe tu consulta: ")
-respuesta_modelo = obtener_respuesta(consulta_usuario)
-print(respuesta_modelo)
+    answer = response.choices[0].text.strip()
+
+    return answer
+
+user_query = input("Enter your query: ")
+model_response = get_response(user_query)
+print(model_response)
